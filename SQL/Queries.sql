4161f1d1735b7a234c86779df34fbb0dc9e3f21c
sharjeelalibcs <sharjeelali.bcs@gmail.com>
2020-04-09 14:32:07 -0400

    updated sql code

--- SQL/Queries.sql
+++ SQL/Queries.sql
@@ -1,3 +1,16 @@
+View the fill_data.sql file for insertion queries. 
+SEARCH QUERIES:
+----------------------------------------------------------------------------------------------------
+get a publisher by name
+select * from publisher
+inner join address on publisher.address_id = address.id
+where name=$1;
+
+--------------------------------------------------
+get a list of every isbn limited to a set length ($1)
+select isbn from book where removed=false order by isbn limit $1
+
+--------------------------------------------------
 regex example. Insenstive case search for "water", "george r r martin"
 select book.isbn,book.title,book.price,book.published_date, author.name as author, genre.name as genre_name from book
 inner join author on author.id = book.author_id 
@@ -8,117 +21,18 @@ and book.published_date like '%2005%'
 and (length('')= 0 or similarity(book.title,'') > 0.15 )
 and (length('robert') = 0 or  similarity(author.name,'chris') > 0.15 )
 and (length('') = 0 or  similarity(publisher.name,'') > 0.15 );
+----------------------------------------------------------------------------------------------------
+SALES QUERIES:
 --------------------------------------------------
+Get total book sales accumlated over time (Using the over keyword), filtered between two dates:
+select date, sum(amount) over (order by date) from
+(select date, sum(amount) as amount from transactions
+where date >=$1 and date<=$2
+group by date ) as daily;
 get 3 most newest books:
 select * from book
 order by published_date DESC
 limit 3;
---------------------------------------------------
-get all book info for a given isbn:
-select book.isbn, book.title, book.description, book.price, book.page_count, book.stock, 
-	   book.rating, book.rating_count, book.published_date, book.add_date, 
-	   author.name as author, genre.name as genre, publisher.name as publisher
-	   from book 
-	   inner join author on book.author_id = author.id 
-	   inner join genre on book.genre_id = genre.id
-	   inner join publisher on book.publisher_id = publisher.id
-where isbn = '9780451207142';
---------------------------------------------------
-Get genre counts for a given search:
-select name, count(*)
-from (select * from book
-where title ILIKE search) as search
-inner join genre on search.genre_id = genre.id
-group by (name);
---------------------------------------------------
-delete all data
-delete from book;
-delete from author;
-delete from publisher;
-delete from genre;
-drop table book;
-drop table author;
-drop table publisher;
-drop table genre;
---------------------------------------------------
-fill test_user:
-insert into client values ('LordofArbiters', 'sharjeelali2552@gmail.com', 'Sharjeel', 'Ali', '12345');
---------------------------------------------------
-fill status:
-insert into status values('0', 'Order Processing', 'Your order is currently being processed. This should only take a few minutes.');
-insert into status values('1', 'Order Preparing', 'Your order is currently being selected from our warehouse, and will be shipped soon.');
-insert into status values('2', 'Shipped', 'Your order has shipped. Expect arrival in the coming days.');
-insert into status values('3', 'Out for Delivery', 'Your order is out for delivery and will arive by the end of the day. ');
-insert into status values('4', 'Delivered', 'Your order has been delivered.');
---------------------------------------------------
-check login info
-select username from client
-where (LOWER(username) = LOWER('ddd') 
-or LOWER(email) = LOWER('randomemail@gmail.com'))
-AND password = '12345';
---------------------------------------------------
-add book to cart:
-insert into cart values('LordofArbiters','9780062498533',3)
-on conflict (username, isbn) do update
-       set quantity = 3
-       where 'LordofArbiters' = cart.username and '9780062498533' = cart.isbn; 
-
---------------------------------------------------
-get cart for a user:
-select book.isbn,book.title,book.price,cart.quantity from book
-inner join cart on book.isbn = cart.isbn
-where cart.username = 'username';
-
---------------------------------------------------
-Add new order:
-insert into order values ('LordofArbiters', '1', 'Mar 23');
-
---------------------------------------------------
-get all orders for a user:
-select orders.order_date, book.isbn, book.title, author.name, book.price, order_book.quantity, order_book.order_number
-from order_book 
-inner join orders on orders.order_number = order_book.order_number 
-inner join book on order_book.isbn = book.isbn
-inner join author on book.author_id = author.id
-where orders.username = 'user'
-order by order_book.order_number desc;
-
- 
-------------------------------------------------
-Insert a new address:
-insert into address values(default,'Canada', 'ON','Ottawa', 'K3Q6M2', 'Exaclior',234, 117);
-insert into client_address values('LordofArbiters', 1);
-------------------------------------------------
-Select all addresses belonging to user:
-select address.country, address.state, address.city, address.code, address.street,address.apt_number from address
-inner join client_address on address.id = client_address.address_id
-where username = 'LordofArbiters';
-------------------------------------------------
-Get most sold books (For home page):
-select book.isbn, book.title, book.price, author.name from
-(select isbn, sum(quantity) as sales from order_book
-group by(isbn) ) as sold
-inner join book on sold.isbn = book.isbn
-inner join author on book.author_id = author.id 
-order by sales desc
-limit 10;
-
-select book.isbn, book.title, book.price,author.name
-from book left join author on author.id = book.author_id
-order by book.add_date DESC
-limit $1;
-------------------------------------------------
-Get most recently viewed books (For home page):
-select book.isbn, book.title, book.price, author.name from
-inner join view_history on book.isbn = view_history.isbn
-inner join book on sold.isbn = book.isbn
-inner join author on book.author_id = author.id 
-where view_history.username = $1
-order by sales desc
-------------------------------------------------
-get recent books for user:
-select book.isbn, book.title, view_history.rank from view_history
-inner join book on book.isbn = view_history.isbn;
 
 ------------------------------------------------
 get sales history:
@@ -126,16 +40,7 @@ select order_date, sum(price*quantity) from orders
 inner join order_book on orders.order_number = order_book.order_number
 inner join book on book.isbn = order_book.isbn
 group by order_date;
-------------------------------------------------
-Populate orders with data:
-SELECT isbn FROM book OFFSET floor(random()*2699) LIMIT 1;
-insert into orders values (default, 'test', '03/02/2020');
 
-
-insert into order_book(isbn, order_number, quantity)
-   select isbn, new.order_number, floor(random() * 5 + 1)::int
-   from book 
-   offset floor(random()*2699) limit 1;
 ------------------------------------------------
 get entire book revenue:
 select sum(price*quantity) from order_book
@@ -177,27 +82,141 @@ on daily.date = transactions.date
 order by date desc
 ;
 ------------------------------------------------
-get all 6 types of sale information
+get all 7 types of sale information as a single number
 select
-	count(case when type='book sale' then 1 else 0 end) as sold,
+    count(case when type='book sale' then 1 else 0 end) as sold,
     sum(amount) as profit,
     sum(case when amount > 0 then amount else 0 end) as sales,
     sum(case when amount < 0 then -amount else 0 end) as expenditures
     sum(case when type ='publisher fees' then -amount else 0 end) as publisher_fees,
     sum(case when type = 'other' then -amount else 0 end) as other
+    sum(case when type = 'restock' then -amount else 0 end) as restock
 from transactions
-------------------------------------------------
-get total sales one after the other per day:
-select date, sum(amount) over (order by date) from 
-(select date, sum(amount) as amount from transactions group by date ) as daily;
 
-------------------------------------------------
-get list of requested books
-select  request_book.request_number, username, request_isbn as isbn, request_title as title, request_book.date as req_date,
-admin_decides.date as desc_date, last_name
-from request_book
+----------------------------------------------------------------------------------------------------
+REQUEST BOOK QUERIES:
+--------------------------------------------------
+Get client request book list:
+select  request_book.request_number, username, request_isbn as isbn, 
+request_title as title, request_book.date  as req_date,
+admin_decides.date as desc_date, decision 
+from request_book 
 left outer join admin_decides on admin_decides.request_number = request_book.request_number
-left outer join admin on admin.email = admin_decides.email;
+where username = $1
+order by request_book.request_number desc
+
+--------------------------------------------------
+Get all book requests:
+select  request_book.request_number, username, request_isbn as isbn, 
+request_title as title, request_book.date  as req_date,
+admin_decides.date as desc_date, last_name, decision
+from request_book "+
+left outer join admin_decides on admin_decides.request_number = request_book.request_number 
+left outer join admin on admin.email = admin_decides.email 
+order by request_book.request_number desc;
+
+----------------------------------------------------------------------------------------------------
+ORDER BOOK QUERIES:
+--------------------------------------------------
+Get all orders for a specific username:
+select orders.order_date, book.isbn, book.title, author.name as author,status.name as status, 
+book.price, order_book.quantity, order_book.order_number, 
+orders.card_number, address.code, address.street, address.unit, address.region,address.city 
+from order_book 
+inner join orders on orders.order_number = order_book.order_number 
+inner join book on order_book.isbn = book.isbn 
+inner join author on book.author_id = author.id 
+inner join address on orders.address_id = address.id
+inner join status on orders.status = status.status_id
+where orders.username = $1
+order by order_book.order_number desc;
+
+----------------------------------------------------------------------------------------------------
+CLIENT HOME PAGE QUERIES:
+--------------------------------------------------
+Get Most sold books:
+select book.isbn, book.title, book.price, author.name as author from 
+(select isbn, sum(quantity) as sales from order_book 
+group by(isbn) ) as sold 
+inner join book on sold.isbn = book.isbn 
+inner join author on book.author_id = author.id 
+order by sales desc 
+limit 10;
+
+--------------------------------------------------
+Get recently viewed books for a user:
+select book.isbn, book.title, book.price, author.name as author from book 
+inner join view_history on book.isbn = view_history.isbn 
+inner join author on book.author_id = author.id  
+where view_history.username = $1 
+order by view_history.rank;
+
+--------------------------------------------------
+Get recently added books sorted by their add date:
+select book.isbn, book.title, book.price,author.name 
+from book left join author on author.id = book.author_id 
+order by book.add_date DESC 
+limit $1;
+
+----------------------------------------------------------------------------------------------------
+CART TAB QUERIES:
+--------------------------------------------------
+Get the list of books and their quantites from the user's cart:
+select book.isbn,book.title,book.price,cart.quantity from book 
+inner join cart on book.isbn = cart.isbn 
+where cart.username = $1;
+
+----------------------------------------------------------------------------------------------------
+BOOK QUERIES:
+--------------------------------------------------
+Get all book information by its isbn
+select book.isbn, book.title, book.description, book.price, book.page_count, book.stock, 
+	   book.rating, book.rating_count, book.published_date, book.add_date, 
+	   author.name as author, genre.name as genre, publisher.name as publisher
+	   from book 
+	   inner join author on book.author_id = author.id 
+	   inner join genre on book.genre_id = genre.id
+	   inner join publisher on book.publisher_id = publisher.id
+where isbn = $1;
+
+--------------------------------------------------
+Remove a book by setting its removed attribute to true:
+update book set removed = true where isbn = $1
+
+----------------------------------------------------------------------------------------------------
+ACCOUNT QUERIES:
+--------------------------------------------------
+Validate that a user's login email is correct
+select email from client where lower(email) = lower($1);
+
+select username from client 
+where (LOWER(username) = LOWER($1) 
+or LOWER(email) = LOWER($1) )
+AND password = $2;
+
+--------------------------------------------------
+Validate that an admin's login information is correct:
+select email from admin 
+where LOWER(email) = LOWER($1) 
+AND password = $2;
+
+--------------------------------------------------
+Get all addresses belonging to a specified username:
+select address.id, address.region, address.city, address.code, 
+address.street, address.unit from address 
+inner join client_address on address.id = client_address.address_id 
+where username = $1;
+
+
+----------------------------------------------------------------------------------------------------
+OTHER QUERIES:
+--------------------------------------------------
+
+Get all payment cards belonging to a specified username:
+select card_info.card_number, card_info.name, card_info.expiry_date from card_info "+
+onner join client_billing on card_info.card_number = client_billing.card_number 
+where username = $1;
+
 
 ------------------------------------------------
 get random number
--- SQL/fill_data.sql
+++ SQL/fill_data.sql
@@ -9,3 +9,85 @@ fill warehouse:
 insert into address_second values('K5D2X9', 'Nepean');
 insert into address_main values(default, 'ON', 'K5D2X9', '45 Northshirien Rd', 4);
 insert into warehouse values(default, 1);
+--------------------------------------------------
+Insert a new client:
+insert into client values ('test', 'test@gmail.com', 'test, 'trst', '12345');
+--------------------------------------------------
+Add new order:
+insert into order values ('LordofArbiters', '1', 'Mar 23');
+------------------------------------------------
+Insert a new address:
+insert into address values(default,'Canada', 'ON','Ottawa', 'K3Q6M2', 'Exaclior',234, 117);
+insert into client_address values('LordofArbiters', 1);
+------------------------------------------------
+Populate orders with data:
+SELECT isbn FROM book OFFSET floor(random()*2699) LIMIT 1;
+insert into orders values (default, 'test', '03/02/2020');
+
+insert into order_book(isbn, order_number, quantity)
+   select isbn, new.order_number, floor(random() * 5 + 1)::int
+   from book 
+   offset floor(random()*2699) limit 1;
+
+------------------------------------------------
+Populate orders with data:
+insert into transaction values (default, $1, $2, $3);
+
+------------------------------------------------
+Populate decides with data:
+insert into admin_decides values ($2, $1, $3, default)
+
+------------------------------------------------
+Populate request with data
+insert into request_book values (default, $1, $2, $3, default)
+
+------------------------------------------------
+Insert a book into the cart or modify it (The conclict inserts if it doesnt exist or else it modifies the quantity):
+insert into cart values($1, $2, $3) 
+on conflict (username, isbn) do update 
+set quantity = $3 
+where $1 = cart.username and $2 = cart.isbn;
+
+------------------------------------------------
+Insert a new book
+insert into book values($1, $2, $3, $4, $5, $6, $7, $8, $9, default, false, $10);
+
+------------------------------------------------
+Insert a new genre
+insert into genre values(default, $1, $2) returning id;
+
+------------------------------------------------
+Insert a new author
+insert into author values(default, $1) returning id;
+
+------------------------------------------------
+Insert a new publisher
+insert into publisher values(default, $1, $2, $3, $4, $5, $6);
+
+------------------------------------------------
+Insert a viewed book into the view_history
+insert into view_history values($1, $2, 0)
+
+------------------------------------------------
+Insert a user's address
+INSERT INTO client_address VALUES ($1, $2);
+
+------------------------------------------------
+Insert a user's payment info:
+INSERT INTO client_billing VALUES ($1, $2);
+
+------------------------------------------------
+Insert a main address info:
+INSERT INTO address_main values (default, $1, $2, $3, $4) RETURNING id;
+
+------------------------------------------------
+Insert a second address (Code, city) info:
+INSERT INTO address_second values ($1, $2);
+
+------------------------------------------------
+Sign up as a new client
+insert into client values ($1, $2, $3, $4, $5);
+
+------------------------------------------------
+Insert a new card
+INSERT INTO card_info values ($1, $2, $3);
